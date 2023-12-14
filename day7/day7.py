9b22a98e9fb841ed03776db542b5900c4c679a56
Rok Nikolič <rok.nikolic@gmail.com>
2023-12-14 11:13:39 +0100

    day 7 part 2 done

--- day7/day7.py
+++ day7/day7.py
@@ -5,43 +5,60 @@ with open('input.txt', 'r') as f:
     puzzle_lines = puzzle_input.split("\n")
 
 
-def get_card_value(card):
+def get_card_value(card, part):
     if card.isnumeric():
         return int(card)
     else:
-        if card == "T": return 10
-        elif card == "J": return 11
-        elif card == "Q": return 12
-        elif card == "K": return 13
-        elif card == "A": return 14
+        if card == "T":
+            return 10
+        elif card == "J":
+            if part == 1:
+                return 11
+            elif part == 2:
+                return 1
+        elif card == "Q":
+            return 12
+        elif card == "K":
+            return 13
+        elif card == "A":
+            return 14
 
 
-def get_hand_strength(hand):
+def determine_hand_type(amounts_of_cards):
+    if amounts_of_cards[0] == 5:
+        return 6
+    elif amounts_of_cards[0] == 4:
+        return 5
+    elif amounts_of_cards[0] == 3 and amounts_of_cards[1] == 2:
+        return 4
+    elif amounts_of_cards[0] == 3 and amounts_of_cards[1] == 1:
+        return 3
+    elif amounts_of_cards[0] == 2 and amounts_of_cards[1] == 2:
+        return 2
+    elif amounts_of_cards[0] == 2 and amounts_of_cards[1] == 1:
+        return 1
+    else:
+        return 0
+
+
+def get_hand_strength(hand, part):
     hand_dict = {}
     card_ratings = []
     for card in hand:
-        card_ratings.append(get_card_value(card))
+        card_ratings.append(get_card_value(card, part))
         if card in hand_dict:
             hand_dict[card] += 1
         else:
             hand_dict[card] = 1
 
-    amounts_of_cards = sorted(hand_dict.values(), reverse=True)
-
-    if amounts_of_cards[0] == 5:
-        hand_strength = 6
-    elif amounts_of_cards[0] == 4:
-        hand_strength = 5
-    elif amounts_of_cards[0] == 3 and amounts_of_cards[1] == 2:
-        hand_strength = 4
-    elif amounts_of_cards[0] == 3 and amounts_of_cards[1] == 1:
-        hand_strength = 3
-    elif amounts_of_cards[0] == 2 and amounts_of_cards[1] == 2:
-        hand_strength = 2
-    elif amounts_of_cards[0] == 2 and amounts_of_cards[1] == 1:
-        hand_strength = 1
+    if part == 2 and "J" in hand_dict and hand_dict["J"] != 5:
+        amount_of_jokers = hand_dict.pop("J")
+        amounts_of_cards = sorted(hand_dict.values(), reverse=True)
+        amounts_of_cards[0] += amount_of_jokers
     else:
-        hand_strength = 0
+        amounts_of_cards = sorted(hand_dict.values(), reverse=True)
+
+    hand_strength = determine_hand_type(amounts_of_cards)
 
     return [hand_strength, card_ratings]
 
@@ -55,11 +72,11 @@ def resolve_same_hands(hand1, hand2):
     return hand2, hand1
 
 
-def day7part1(lines):
+def day7(lines, part):
     hands = []
     for line in lines:
         hand, bid = line.split()
-        hands.append(get_hand_strength(hand) + [int(bid)])
+        hands.append(get_hand_strength(hand, part) + [int(bid)])
 
     sorted_hands = sorted(hands)
 
@@ -79,16 +96,12 @@ def day7part1(lines):
     return total_sum
 
 
-def day7part2(lines):
-    return 0
-
-
 start = time.perf_counter()
-result = day7part1(puzzle_lines)
+result = day7(puzzle_lines, 1)
 end = time.perf_counter()
 print(f"Day 7 Part 1 result is: {result}, computed in: {end - start} seconds")
 
 start = time.perf_counter()
-result = day7part2(puzzle_lines)
+result = day7(puzzle_lines, 2)
 end = time.perf_counter()
 print(f"Day 7 Part 2 result is: {result}, computed in: {end - start} seconds")

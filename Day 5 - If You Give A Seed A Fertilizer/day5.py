f6f3a5ee2b95523cc594e20aaad638292c492301
Rok Nikolič <rok.nikolic@gmail.com>
2023-12-31 16:04:06 +0100

    day 5 finished but not polished yet :)

--- Day 5 - If You Give A Seed A Fertilizer/day5.py
+++ Day 5 - If You Give A Seed A Fertilizer/day5.py
@@ -3,7 +3,7 @@ import time
 
 def create_range(mapping):
     dest, start_of_range, length = map(int, mapping.split())
-    end_of_range = start_of_range + length - 1
+    end_of_range = start_of_range + length
     offset = dest - start_of_range
     return [start_of_range, end_of_range, offset]
 
@@ -19,20 +19,50 @@ def apply_maps(seed, map_list):
 
 
 def part1(lines):
-    seed_map = lines.pop(0)
+    seed_map, *line_maps = lines
     seed_list = list(map(int, seed_map.split()[1:]))
 
-    map_list = []
-    for line in lines:
+    almanac = []
+    for line in line_maps:
         _, *maps = line.split("\n")
-        map_list.append([create_range(mapping) for mapping in maps])
+        ranges = [create_range(mapping) for mapping in maps]
+        almanac.append(ranges)
 
-    location_list = [apply_maps(seed, map_list) for seed in seed_list]
+    location_list = [apply_maps(seed, almanac) for seed in seed_list]
     return min(location_list)
 
 
 def part2(lines):
-    return 0
+    seed_map, *line_maps = lines
+    seed_list = list(map(int, seed_map.split()[1:]))
+
+    seed_pairs = []
+    for i in range(0, len(seed_list), 2):
+        seed_pairs.append((seed_list[i], seed_list[i] + seed_list[i + 1]))
+
+    for line in line_maps:
+        _, *maps = line.split("\n")
+        ranges = [create_range(mapping) for mapping in maps]
+
+        locations = []
+        while seed_pairs:
+            seed_start, seed_end = seed_pairs.pop()
+            for start_interval, end_interval, offset in ranges:
+                overlap_start = max(seed_start, start_interval)
+                overlap_end = min(seed_end, end_interval)
+                if overlap_start < overlap_end:
+                    locations.append((overlap_start + offset, overlap_end + offset))
+                    if overlap_start > seed_start:
+                        seed_pairs.append((seed_start, overlap_start))
+                    if seed_end > overlap_end:
+                        seed_pairs.append((overlap_end, seed_end))
+                    break
+            else:
+                locations.append((seed_start, seed_end))
+
+        seed_pairs = locations
+
+    return min(seed_pairs)[0]
 
 
 if __name__ == '__main__':

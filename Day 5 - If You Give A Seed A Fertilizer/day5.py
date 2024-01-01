149d6353b53ba06106c1ac7cd75e6037d34027da
Rok Nikolič <rok.nikolic@gmail.com>
2024-01-01 22:29:42 +0100

    day 5 refactored

--- Day 5 - If You Give A Seed A Fertilizer/day5.py
+++ Day 5 - If You Give A Seed A Fertilizer/day5.py
@@ -8,14 +8,35 @@ def create_range(mapping):
     return [start_of_range, end_of_range, offset]
 
 
-def apply_maps(seed, map_list):
-    for maps in map_list:
-        for map_range in maps:
-            if seed in range(map_range[0], map_range[1] + 1):
-                seed += map_range[2]
+def apply_maps(seed, almanac):
+    for maps in almanac:
+        for start_interval, end_interval, offset in maps:
+            if seed in range(start_interval, end_interval):
+                seed += offset
                 break
-
     return seed
+
+
+def apply_map_ranges(seed_pairs, almanac):
+    for maps in almanac:
+        locations = []
+        while seed_pairs:
+            seed_start, seed_end = seed_pairs.pop()
+            for start_interval, end_interval, offset in maps:
+                overlap_start = max(seed_start, start_interval)
+                overlap_end = min(seed_end, end_interval)
+                if overlap_start < overlap_end:
+                    locations.append((overlap_start + offset, overlap_end + offset))
+                    if overlap_start > seed_start:
+                        seed_pairs.append((seed_start, overlap_start))
+                    if seed_end > overlap_end:
+                        seed_pairs.append((overlap_end, seed_end))
+                    break
+            else:
+                locations.append((seed_start, seed_end))
+        seed_pairs = locations
+
+    return seed_pairs
 
 
 def part1(lines):
@@ -28,8 +49,8 @@ def part1(lines):
         ranges = [create_range(mapping) for mapping in maps]
         almanac.append(ranges)
 
-    location_list = [apply_maps(seed, almanac) for seed in seed_list]
-    return min(location_list)
+    locations = [apply_maps(seed, almanac) for seed in seed_list]
+    return min(locations)
 
 
 def part2(lines):
@@ -40,29 +61,15 @@ def part2(lines):
     for i in range(0, len(seed_list), 2):
         seed_pairs.append((seed_list[i], seed_list[i] + seed_list[i + 1]))
 
+    almanac = []
     for line in line_maps:
         _, *maps = line.split("\n")
         ranges = [create_range(mapping) for mapping in maps]
+        almanac.append(ranges)
 
-        locations = []
-        while seed_pairs:
-            seed_start, seed_end = seed_pairs.pop()
-            for start_interval, end_interval, offset in ranges:
-                overlap_start = max(seed_start, start_interval)
-                overlap_end = min(seed_end, end_interval)
-                if overlap_start < overlap_end:
-                    locations.append((overlap_start + offset, overlap_end + offset))
-                    if overlap_start > seed_start:
-                        seed_pairs.append((seed_start, overlap_start))
-                    if seed_end > overlap_end:
-                        seed_pairs.append((overlap_end, seed_end))
-                    break
-            else:
-                locations.append((seed_start, seed_end))
+    locations = apply_map_ranges(seed_pairs, almanac)
 
-        seed_pairs = locations
-
-    return min(seed_pairs)[0]
+    return min(locations)[0]
 
 
 if __name__ == '__main__':

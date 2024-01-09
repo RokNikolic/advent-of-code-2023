ea424d603b540b9db3f1c4453056d03feee883b4
Rok Nikolič <rok.nikolic@gmail.com>
2024-01-09 01:01:35 +0100

    day 13 part 1 done but not working, idk why

--- Python/Day 13 - Point of Incidence.py
+++ Python/Day 13 - Point of Incidence.py
@@ -1,8 +1,38 @@
 import time
 
 
-def part1(lines):
-    return 0
+def find_mirror(array):
+    last_found = []
+    for i, line in enumerate(array):
+        if line == last_found:
+            real_reflection_flag = True
+            for j in range(min(i, len(array) - i)):
+                real_reflection_flag = array[i - (j + 1)] == array[i + j]
+                print(array[i - (j + 1)], array[i + j])
+
+            if real_reflection_flag:
+                print(i)
+                return i
+        else:
+            last_found = line
+    else:
+        return None
+
+
+def part1(chunks):
+    row_sum = 0
+    column_sum = 0
+    for chunk in chunks:
+        array = chunk.split()
+        transposed_array = [col for col in zip(*array)]
+
+        if vertical_reflection := find_mirror(array):
+            row_sum += vertical_reflection
+
+        elif horizontal_reflection := find_mirror(transposed_array):
+            column_sum += horizontal_reflection
+
+    return column_sum + (row_sum * 100)
 
 
 def part2(lines):
@@ -12,14 +42,14 @@ def part2(lines):
 if __name__ == "__main__":
     with open(r'../Input/day13.txt', 'r') as f:
         puzzle_input = f.read()
-        puzzle_lines = puzzle_input.split("\n")
+        puzzle_chunks = puzzle_input.split("\n\n")
 
     start = time.perf_counter()
-    result = part1(puzzle_lines)
+    result = part1(puzzle_chunks)
     end = time.perf_counter()
     print(f"Part 1 result is: {result}, computed in: {end - start :.3} seconds")
 
     start = time.perf_counter()
-    result = part2(puzzle_lines)
+    result = part2(puzzle_chunks)
     end = time.perf_counter()
     print(f"Part 2 result is: {result}, computed in: {end - start :.3} seconds")

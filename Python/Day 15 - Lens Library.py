1d7619f402d88f9a8eafd72df0f9836bc3b03ddb
Rok Nikolič <rok.nikolic@gmail.com>
2024-01-20 22:01:17 +0100

    day 15 done init

---
+++ Python/Day 15 - Lens Library.py
@@ -0,0 +1,25 @@
+import time
+
+
+def part1(lines):
+    return 0
+
+
+def part2(lines):
+    return 0
+
+
+if __name__ == "__main__":
+    with open(r'../Input/day15.txt', 'r') as f:
+        puzzle_input = f.read()
+        puzzle_lines = puzzle_input.split("\n")
+
+    start = time.perf_counter()
+    result = part1(puzzle_lines)
+    end = time.perf_counter()
+    print(f"Part 1 result is: {result}, computed in: {end - start :.3} seconds")
+
+    start = time.perf_counter()
+    result = part2(puzzle_lines)
+    end = time.perf_counter()
+    print(f"Part 2 result is: {result}, computed in: {end - start :.3} seconds")

77aae1a0e65ad8c473d8d9ec24f17868486df1f9
Rok Nikolič <rok.nikolic@gmail.com>
2024-01-21 00:21:40 +0100

    day 15 part 1 done

--- Python/Day 15 - Lens Library.py
+++ Python/Day 15 - Lens Library.py
@@ -1,8 +1,19 @@
 import time
 
 
-def part1(lines):
-    return 0
+def process_string(string):
+    current_value = 0
+    for character in string:
+        current_value += ord(character)
+        current_value = (current_value * 17) % 256
+    return current_value
+
+
+def part1(strings):
+    total_sum = 0
+    for string in strings:
+        total_sum += process_string(string)
+    return total_sum
 
 
 def part2(lines):
@@ -12,14 +23,14 @@ def part2(lines):
 if __name__ == "__main__":
     with open(r'../Input/day15.txt', 'r') as f:
         puzzle_input = f.read()
-        puzzle_lines = puzzle_input.split("\n")
+        puzzle_strings = puzzle_input.split(",")
 
     start = time.perf_counter()
-    result = part1(puzzle_lines)
+    result = part1(puzzle_strings)
     end = time.perf_counter()
     print(f"Part 1 result is: {result}, computed in: {end - start :.3} seconds")
 
     start = time.perf_counter()
-    result = part2(puzzle_lines)
+    result = part2(puzzle_strings)
     end = time.perf_counter()
     print(f"Part 2 result is: {result}, computed in: {end - start :.3} seconds")

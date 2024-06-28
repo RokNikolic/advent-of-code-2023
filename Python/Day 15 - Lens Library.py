2d1f2bdd632b8676d1b0b1426b04eaa164caf170
Rok Nikolič <rok.nikolic@gmail.com>
2024-06-28 11:32:22 +0200

    day 15 done

--- Python/Day 15 - Lens Library.py
+++ Python/Day 15 - Lens Library.py
@@ -10,14 +10,42 @@ def hash_algorithm(string):
 
 
 def part1(puzzle_input):
-    strings = puzzle_input.split(",")
-    total_sum = sum(map(hash_algorithm, strings))
+    steps = puzzle_input.split(",")
+    total_sum = sum(map(hash_algorithm, steps))
     return total_sum
 
 
+def remove_lens_from_box(boxes, box_index, lens):
+    working_box = boxes[box_index]
+    if lens in working_box:
+        del working_box[lens]
+
+
+def add_lens_to_box(boxes, box_index, lens, focal_length):
+    working_box = boxes[box_index]
+    working_box[lens] = focal_length
+
+
 def part2(puzzle_input):
-    strings = puzzle_input.split(",")
-    return 0
+    steps = puzzle_input.split(",")
+    boxes = [{} for _ in range(256)]
+    for step in steps:
+        if "-" in step:
+            lens_label = step.replace('-', '')
+            box_index = hash_algorithm(lens_label)
+            remove_lens_from_box(boxes, box_index, lens_label)
+        elif "=" in step:
+            lens_label, focal_length = step.split("=")
+            box_index = hash_algorithm(lens_label)
+            add_lens_to_box(boxes, box_index, lens_label, focal_length)
+
+    total_sum = 0
+    for i, box in enumerate(boxes):
+        box_number = i + 1
+        for j, focal_length in enumerate(box.values()):
+            slot_number = j + 1
+            total_sum += box_number * slot_number * int(focal_length)
+    return total_sum
 
 
 if __name__ == "__main__":

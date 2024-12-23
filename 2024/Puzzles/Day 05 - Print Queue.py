9ae5887235fbd052dd390ba2d2df6035eb263747
Rok Nikolic <rok.nikolic@gmail.com>
2024-12-23 16:15:06 +0100

    par 2 init, sorting tbd

--- 2024/Puzzles/Day 05 - Print Queue.py
+++ 2024/Puzzles/Day 05 - Print Queue.py
@@ -1,18 +1,22 @@
 import time
 
 
-def check_update_correctness(update_list, order_dictionary):
-    for i, page_number in enumerate(update_list):
-        if page_number not in order_dictionary:
+def check_update_correctness(update, preceding_number_dictionary):
+    for i, page_number in enumerate(update):
+        if page_number not in preceding_number_dictionary:
             if i != 0:
                 return False
         else:
-            previous_needed_numbers = order_dictionary[page_number]
-            previous_order_numbers = set(update_list[:i])
+            previous_needed_numbers = preceding_number_dictionary[page_number]
+            previous_order_numbers = set(update[:i])
             if not previous_order_numbers.issubset(previous_needed_numbers):
                 return False
     else:
         return True
+
+
+def order_update(update, order_dictionary):
+    pass
 
 
 def part1(puzzle_input):
@@ -20,27 +24,42 @@ def part1(puzzle_input):
     orders = order_part.split("\n")
     updates = update_part.split("\n")
 
-    order_dict = {}
+    preceding_number_dict = {}
     for order in orders:
         before, after = list(map(int, order.split("|")))
-        if after not in order_dict:
-            order_dict[after] = {before}
+        if after not in preceding_number_dict:
+            preceding_number_dict[after] = {before}
         else:
-            order_dict[after].add(before)
+            preceding_number_dict[after].add(before)
 
     overall_sum = 0
     for update in updates:
-        page_numbers = list(map(int, update.split(",")))
-        if check_update_correctness(page_numbers, order_dict):
-            middle_index = len(page_numbers) // 2
-            overall_sum += page_numbers[middle_index]
-    
+        update_numbers = list(map(int, update.split(",")))
+        if check_update_correctness(update_numbers, preceding_number_dict):
+            overall_sum += update_numbers[len(update_numbers) // 2]
     return overall_sum
 
 
 def part2(puzzle_input):
+    order_part, update_part = puzzle_input.split("\n\n")
+    orders = order_part.split("\n")
+    updates = update_part.split("\n")
 
-    return 0
+    preceding_number_dict = {}
+    for order in orders:
+        before, after = list(map(int, order.split("|")))
+        if after not in preceding_number_dict:
+            preceding_number_dict[after] = {before}
+        else:
+            preceding_number_dict[after].add(before)
+
+    overall_sum = 0
+    for update in updates:
+        update_numbers = list(map(int, update.split(",")))
+        if not check_update_correctness(update_numbers, preceding_number_dict):
+            order_update(update_numbers)
+            overall_sum += update_numbers[len(update_numbers) // 2]
+    return overall_sum
 
 
 if __name__ == "__main__":

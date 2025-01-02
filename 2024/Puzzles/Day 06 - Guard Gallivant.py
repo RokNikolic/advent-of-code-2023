7d374423222379ad2fa62cc1a747e50d868b4956
Rok Nikolič <rok.nikolic@gmail.com>
2025-01-02 17:33:21 +0100

    day 6 part 1 done

--- 2024/Puzzles/Day 06 - Guard Gallivant.py
+++ 2024/Puzzles/Day 06 - Guard Gallivant.py
@@ -2,18 +2,32 @@ import time
 
 
 def find_guard(matrix):
+    directions = {"^":(-1, 0), ">":(1, 0), "ˇ":(0, 1), "<":(0, -1)}
     for y in range(len(matrix)):
         for x in range(len(matrix[0])):
             if matrix[y][x] in "^ˇ><":
-                return (y, x, matrix[y][x])
-    return 0
+                return (y, x), directions[matrix[y][x]]
 
+
+def rotate_right(position):
+    direction_change = {(-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0)}
+    return direction_change[position]
 
 
 def part1(puzzle_input):
     matrix = puzzle_input.split("\n")
-    print(find_guard(matrix))
-    return 0
+    current_position, current_direction = find_guard(matrix)
+
+    visited_positions = [current_position]
+    while current_position[0] + 1 < len(matrix[0]) and current_position[1] + 1 < len(matrix[1]):
+        new_position = (current_position[0] + current_direction[0], current_position[1] + current_direction[1])
+        if matrix[new_position[0]][new_position[1]] == "#":
+            current_direction = rotate_right(current_direction)
+        else:
+            current_position = new_position
+            visited_positions.append(new_position)
+
+    return len(set(visited_positions))
 
 
 def part2(puzzle_input):

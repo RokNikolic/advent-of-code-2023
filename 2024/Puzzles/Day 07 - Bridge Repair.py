135c1764ea31f37c6a42ec0605dbe61a75d4c57b
Rok Nikolič <rok.nikolic@gmail.com>
2025-01-10 21:53:32 +0100

    day 7 part 2 done

--- 2024/Puzzles/Day 07 - Bridge Repair.py
+++ 2024/Puzzles/Day 07 - Bridge Repair.py
@@ -1,14 +1,31 @@
 import time
 
 
-def determine_calculability(product, numbers):
+def determine_cal(product, numbers):
     last_number = numbers[-1]
     the_rest_of_nums = numbers[:-1]
     if len(numbers) == 1:
         return product == last_number
-    if product % last_number == 0 and determine_calculability(product / last_number, the_rest_of_nums):
+    if product % last_number == 0 and determine_cal(product // last_number, the_rest_of_nums):
         return True
-    if product > last_number and determine_calculability(product - last_number, the_rest_of_nums):
+    if product > last_number and determine_cal(product - last_number, the_rest_of_nums):
+        return True
+    else:
+        return False
+
+
+def determine_cal_cot(product, numbers):
+    last_number = numbers[-1]
+    the_rest_of_nums = numbers[:-1]
+    if len(numbers) == 1:
+        return product == last_number
+    if product % last_number == 0 and determine_cal_cot(product // last_number, the_rest_of_nums):
+        return True
+    if product > last_number and determine_cal_cot(product - last_number, the_rest_of_nums):
+        return True
+    p = str(product)
+    l_n= str(last_number)
+    if len(p) > len(l_n) and p.endswith(l_n) and determine_cal_cot(int(p[:-len(l_n)]), the_rest_of_nums):
         return True
     else:
         return False
@@ -21,14 +38,21 @@ def part1(puzzle_input):
         product_str, numbers_str = line.split(": ")
         product = int(product_str)
         numbers = [int(num) for num in numbers_str.split()]
-        if determine_calculability(product, numbers):
+        if determine_cal(product, numbers):
             overall_sum += product
-
     return overall_sum
 
 
 def part2(puzzle_input):
-    return 0
+    lines = puzzle_input.split("\n")
+    overall_sum = 0
+    for line in lines:
+        product_str, numbers_str = line.split(": ")
+        product = int(product_str)
+        numbers = [int(num) for num in numbers_str.split()]
+        if determine_cal_cot(product, numbers):
+            overall_sum += product
+    return overall_sum
 
 
 if __name__ == "__main__":

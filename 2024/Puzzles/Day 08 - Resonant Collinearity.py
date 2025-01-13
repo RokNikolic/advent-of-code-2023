9aff16cecfe73ceac84f69a92655306028e168d6
Rok Nikolič <rok.nikolic@gmail.com>
2025-01-13 16:22:06 +0100

    day 8 part 1 done

--- 2024/Puzzles/Day 08 - Resonant Collinearity.py
+++ 2024/Puzzles/Day 08 - Resonant Collinearity.py
@@ -1,39 +1,42 @@
-import math
 import time
 
 
-def part1(puzzle_input):
-    matrix = puzzle_input.split("\n")
+def make_antenna_dict(matrix):
     antenna_dict = {}
-    for y in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            if matrix[y][x] != ".":
-                antenna = matrix[y][x]
+    for y, row in enumerate(matrix):
+        for x, antenna in enumerate(row):
+            if antenna != ".":
                 if antenna not in antenna_dict:
                     antenna_dict[antenna] = [(y, x)]
                 else:
                     antenna_dict[antenna].append((y, x))
+    return antenna_dict
 
-    node_set = set()
+
+def get_anti_nodes(point1, point2):
+    y1, x1 = point1
+    y2, x2 = point2
+    dist_y, dist_x = (y1 - y2, x1 - x2)
+    node1 = (y1 + dist_y, x1 + dist_x)
+    node2 = (y2 - dist_y, x2 - dist_x)
+    return node1, node2
+
+
+def part1(puzzle_input):
+    matrix = puzzle_input.split("\n")
+    antenna_dict = make_antenna_dict(matrix)
+
+    anti_node_set = set()
     for points in antenna_dict.values():
         for i in range(len(points)):
             for j in range(i+1, len(points)):
-                y1, x1 = points[i]
-                y2, x2 = points[j]
-                vector = (x2 - x1,y2 - y1)
-                distance = math.dist(points[i], points[j])
-                normalized_vector = (vector[0] / distance, vector[1] / distance)
+                node1, node2 = get_anti_nodes(points[i], points[j])
+                if 0 <= node1[0] < len(matrix) and 0 <= node1[1] < len(matrix[0]):
+                    anti_node_set.add(node1)
+                if 0 <= node2[0] < len(matrix) and 0 <= node2[1] < len(matrix[0]):
+                    anti_node_set.add(node2)
 
-                offset1_y, offset1_x = (normalized_vector[0] * -distance, normalized_vector[1] * -distance)
-                offset2_y, offset2_x = (normalized_vector[0] * 2 * distance, normalized_vector[1] * 2 * distance)
-                node1 = (y1 + offset1_y, x1 + offset1_x)
-                node2 = (y1 + offset2_y, x1 + offset2_x)
-                if node1[0] >= 0 and node1[1] >= 0:
-                    node_set.add(node1)
-                if node2[0] >= 0 and node2[1] >= 0:
-                    node_set.add(node2)
-    print(node_set)
-    return len(node_set)
+    return len(anti_node_set)
 
 
 def part2(puzzle_input):

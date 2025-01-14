555ea0c30cd5ce6d67a5c4842ec8d336ca26b19a
Rok Nikolič <rok.nikolic@gmail.com>
2025-01-14 18:33:24 +0100

    day 8 done

--- 2024/Puzzles/Day 08 - Resonant Collinearity.py
+++ 2024/Puzzles/Day 08 - Resonant Collinearity.py
@@ -13,35 +13,66 @@ def make_antenna_dict(matrix):
     return antenna_dict
 
 
-def get_anti_nodes(point1, point2):
+def get_2_anti_nodes(point1, point2, borders):
+    y1, x1 = point1
+    y2, x2 = point2
+    dist_y, dist_x = (y2 - y1, x2 - x1)
+    height, width = borders
+    nodes = []
+    node1 = (y1 - dist_y, x1 - dist_x)
+    if 0 <= node1[0] < height and 0 <= node1[1] < width:
+        nodes.append(node1)
+    node2 = (y2 + dist_y, x2 + dist_x)
+    if 0 <= node2[0] < height and 0 <= node2[1] < width:
+        nodes.append(node2)
+    return nodes
+
+
+def get_all_anti_nodes(point1, point2, borders):
     y1, x1 = point1
     y2, x2 = point2
     dist_y, dist_x = (y1 - y2, x1 - x2)
-    node1 = (y1 + dist_y, x1 + dist_x)
-    node2 = (y2 - dist_y, x2 - dist_x)
-    return node1, node2
+    height, width = borders
+    nodes = []
+    for i in range(1, 1000):
+        node1 = (y1 - dist_y * i, x1 - dist_x * i)
+        if 0 <= node1[0] < height and 0 <= node1[1] < width:
+            nodes.append(node1)
+        else:
+            break
+    for i in range(1, 1000):
+        node2 = (y2 + dist_y * i, x2 + dist_x * i)
+        if 0 <= node2[0] < height and 0 <= node2[1] < width:
+            nodes.append(node2)
+        else:
+            break
+    return nodes
 
 
 def part1(puzzle_input):
     matrix = puzzle_input.split("\n")
     antenna_dict = make_antenna_dict(matrix)
 
-    anti_node_set = set()
+    anti_nodes = []
     for points in antenna_dict.values():
         for i in range(len(points)):
             for j in range(i+1, len(points)):
-                node1, node2 = get_anti_nodes(points[i], points[j])
-                if 0 <= node1[0] < len(matrix) and 0 <= node1[1] < len(matrix[0]):
-                    anti_node_set.add(node1)
-                if 0 <= node2[0] < len(matrix) and 0 <= node2[1] < len(matrix[0]):
-                    anti_node_set.add(node2)
-
-    return len(anti_node_set)
+                nodes = get_2_anti_nodes(points[i], points[j], (len(matrix), len(matrix[0])))
+                anti_nodes.extend(nodes)
+    return len(set(anti_nodes))
 
 
 def part2(puzzle_input):
-    lines = puzzle_input.split("\n")
-    return 0
+    matrix = puzzle_input.split("\n")
+    antenna_dict = make_antenna_dict(matrix)
+
+    anti_nodes = []
+    for points in antenna_dict.values():
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+                nodes = get_all_anti_nodes(points[i], points[j], (len(matrix), len(matrix[0])))
+                anti_nodes.extend(nodes)
+    return len(set(anti_nodes))
 
 
 if __name__ == "__main__":
